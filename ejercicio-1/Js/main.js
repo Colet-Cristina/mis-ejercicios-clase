e297411d59a617d0b8f88354004cd5d906088701
Cristina <tu-emailejemplo.com>
2026-02-13 13:14:04 +0100

    Adding exercises

---
+++ ejercicio-1/Js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+document.querySelector('h1').innerHTML = 'Hola mundo';
+
+const mainElement = document.querySelector('main');
+const content = '<li class="name">Marta</li>';
+mainElement.innerHTML = content;
